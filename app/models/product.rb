3ebe569c7244e615eeadf0447c0c2af667e34686
langdag <lucky10cents@gmail.com>
2017-03-14 19:09:14 +0200

    Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,23 @@ class Product < ApplicationRecord
 		with:  %r{\.(gif|jpg|png)\Z}i,
 		message: 'must be a URL for GIF, JPG or PNG image.'
 	} 
+	validates  :title, length: {
+		minimum: 10, 
+		too_short: " must be %{count} characters long"}
+
+	has_many :line_items
+
+	before_destroy :ensure_not_referenced_by_any_line_item
+
+#.........
+	private
+
+		def ensure_not_referenced_by_any_line_item
+			unless line_items.empty?
+				errors.add(:base, 'Line items present')
+				throw :abort
+			end
+		end
+
+
 end
