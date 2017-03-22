c6ed3fee39aa1ce96345cef96a27eb6207d002e6
langdag <lucky10cents@gmail.com>
2017-03-22 17:09:17 +0200

    created an Ajax dased cart, also added js-ui effect blind, retemplated layouts with partials

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -23,4 +23,15 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 		assert_redirected_to line_item_url(@line_item.product_id)
 	end
 
+	test "should create line_item via ajax" do
+		assert_difference('LineItem.count') do
+			post line_items_url, params: { product_id: products(:ruby).id },
+				xhr: true
+		end
+
+		assert_response :success
+		assert_select_jquery :html, '#cart' do
+			assert_select 'tr#current_item td', /Programming Ruby 1.9/
+		end
+	end
 end
