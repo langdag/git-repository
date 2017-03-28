7510eaad5b4050a119f2007dbc3af78e15352ae8
langdag <lucky10cents@gmail.com>
2017-03-28 15:00:29 +0300

    added form for orders,some style css,new fixtures, and validations for the form,made associations for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -29,10 +29,15 @@ class OrdersController < ApplicationController
   # POST /orders.json
   def create
     @order = Order.new(order_params)
+    @order.add_line_items_from_cart(@cart)
 
     respond_to do |format|
       if @order.save
-        format.html { redirect_to @order, notice: 'Order was successfully created.' }
+        Cart.destroy(session[:cart_id])
+        session[:cart_id] = nil
+        
+        format.html { redirect_to store_index_url,
+         notice: 'Thank you for your order.' }
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new }
@@ -79,6 +84,6 @@ class OrdersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
-      params.require(:order).permit(:name, :address, :email, : pay_type)
+      params.require(:order).permit(:name, :address, :email, :pay_type)
     end
 end
---
+++ test/controllers/orders_controller_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class OrdersControllerTest < ActionDispatch::IntegrationTest
+
+	test "requires item in cart" do
+		get new_order_url
+		assert_redirected_to store_index_path
+		assert_equal flash[:notice], 'Your cart is empty'
+	end
+
+	test "should get new" do
+		post line_items_url, params: { product_id: products(:ruby).id }
+		
+		get new_order_url
+		assert_response :success
+	end
+
+	test "should create order" do
+		assert_difference('Order.count') do
+			post orders_url, params: { order: { address: @order.address,
+			email: @order.email, name: @order.name,
+			pay_type: @order.pay_type } }
+		end
+
+		assert_redirected_to store_index_url
+	end
+end
